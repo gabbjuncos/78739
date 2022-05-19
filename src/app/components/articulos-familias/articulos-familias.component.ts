a7cf5a7a92435359299e96a6b928e1f43cacd762
gabbjuncos <gabbjuncos@gmail.com>
2022-05-19 16:58:57 -0300

    1 file modified

--- src/app/components/articulos-familias/articulos-familias.component.ts
+++ src/app/components/articulos-familias/articulos-familias.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  ArticuloFamilia,
+  ArticulosFamilias,
+} from '../../models/articulo-familia';
 
 @Component({
   selector: 'app-articulos-familias',
   templateUrl: './articulos-familias.component.html',
   styleUrls: ['./articulos-familias.component.css'],
 })
-export class ArticulosFamiliasComponent {
-  Titulo = 'Pymes Demo - 2021';
+export class ArticulosFamiliasComponent implements OnInit {
+  Items = ArticulosFamilias;
+  Titulo = 'Articulos Familias';
+  constructor() {}
+
+  ngOnInit() {}
 }
